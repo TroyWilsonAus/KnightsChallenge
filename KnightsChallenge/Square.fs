5504272783c2dd4c08ed6b807a5a204c588ceca6
TroyWilsonAus <troyboy.wilson@gmail.com>
2015-09-28 17:49:01 +1000

    added ChangeStatus method

--- KnightsChallenge/Square.fs
+++ KnightsChallenge/Square.fs
@@ -6,6 +6,8 @@ type Square (row: int, column: int, status: SquareStatus) =
     member self.Row = row
     member self.Column = column
     member self.Status = status
+    member self.ChangeStatus status = 
+        new Square(self.Row, self.Column, status)
 
 [<CustomEquality>]
 type PossibleSquare (row: int, column: int) =
