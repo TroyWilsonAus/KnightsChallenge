17e154ccb669c536c89a337a5948f494e361961f
TroyWilsonAus <troyboy.wilson@gmail.com>
2015-10-07 21:29:28 +1100

    added delay function

--- KnightsChallenge/Util.fs
+++ KnightsChallenge/Util.fs
@@ -1,7 +1,11 @@
 ﻿module Util
 
+let delayFunc = fun _ ->
+    System.Threading.Thread.CurrentThread.Join 500 |> ignore
+
 let logFun text arg =
     let retval = printfn text arg
     
     System.Threading.Thread.CurrentThread.Join 1000 |> ignore
     retval |> ignore
+
