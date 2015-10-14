0cd647acc2e91561820888ee05181c2689143976
Troy Wilson <twilson@aconex.com>
2015-10-14 19:42:24 +1100

    created check square function, possible in preperation for multi threading

--- KnightsChallenge/Program.fs
+++ KnightsChallenge/Program.fs
@@ -14,31 +14,32 @@ let LogFailure = fun(s : Square) ->
 
 let LogSuccess = fun(s: Square, b :PlayingBoard) ->
     logFun "Used all squares starting at Row: %d, Col: %d" s.Row, s.Column
-    
 
+let CheckSquare = fun(s: Square) -> 
+    let res = logFun "Creating playing board %s" "test"
+    let board = new PlayingBoard()
+    let mutable piece = board.SetStartPosition s.Row s.Column
+    board.DrawBoard()
+    let mutable move = board.GetNextMove(piece)
+    while (move.IsSome) do                        
+        piece <- board.PerformMove(move.Value)
+        piece.LogPosition |> ignore
+        board.DrawBoard()
+
+        move <- board.GetNextMove(piece)
+        
+    match board.AllSquaresUsed() with
+        | true  -> LogSuccess(s, board) |> ignore
+        | false -> LogFailure(s)  |> ignore
+    
+        
 [<EntryPoint>]
 let main argv = 
     
     let allSquares = Board.createDefaultSquares
 
     for s in allSquares do
-        let res = logFun "Creating playing board %s" "test"
-        let board = new PlayingBoard()
-        let mutable piece = board.SetStartPosition s.Row s.Column
-        board.DrawBoard()
-        let mutable move = board.GetNextMove(piece)
-        while (move.IsSome) do                        
-            piece <- board.PerformMove(move.Value)
-            piece.LogPosition |> ignore
-            board.DrawBoard()
-
-            move <- board.GetNextMove(piece)
-        
-        match board.AllSquaresUsed() with
-            | true  -> LogSuccess(s, board) |> ignore
-            | false -> LogFailure(s) |> ignore
-            
-            
-            
+        CheckSquare(s)
+                    
 
     0 // return an integer exit code
